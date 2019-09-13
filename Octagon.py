4267615dd5ce733e8f23e66dda6806750eae1600
Xander <alexander.eagle@ssfs.org>
2019-09-13 11:18:32 -0400

    added octagon w color

--- Octagon.py
+++ Octagon.py
@@ -1,2 +1,14 @@
 import turtle
-turtle.
+
+turtle.fillcolor("maroon")
+turtle.begin_fill()
+
+for x in range(8):
+    turtle.forward(70)
+    turtle.left(45)
+
+turtle.end_fill()
+
+
+
+turtle.exitonclick()

be5cbc06019c5c805779bd03e395e3a6de836736
Xander <alexander.eagle@ssfs.org>
2019-09-16 12:05:07 -0400

    added octagon w color

--- Octagon.py
+++ Octagon.py
@@ -1,14 +1,43 @@
 import turtle
 
+def makeaoctagon():
+    for x in range(8):
+        turtle.forward(70)
+        turtle.left(45)
+
 turtle.fillcolor("maroon")
 turtle.begin_fill()
-
-for x in range(8):
-    turtle.forward(70)
-    turtle.left(45)
-
+makeaoctagon()
 turtle.end_fill()
 
+turtle.up()
+turtle.goto(200,0)
+
+
+turtle.fillcolor("teal")
+turtle.begin_fill()
+makeaoctagon()
+turtle.end_fill()
+
+
+turtle.up()
+turtle.goto(400,0)
+
+
+turtle.fillcolor("yellow")
+turtle.begin_fill()
+makeaoctagon()
+turtle.end_fill()
+
+
+turtle.up()
+turtle.goto(600,0)
+
+
+turtle.fillcolor("purple")
+turtle.begin_fill()
+makeaoctagon()
+turtle.end_fill()
 
 
 turtle.exitonclick()
